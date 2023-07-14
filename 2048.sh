e54ec4243c07405a31873d03685755eea00bdd3b
narshee <76812090+narshee@users.noreply.github.com>
2023-07-14 10:38:02 +0000

    add highscore, score increase & cleanup

--- 2048.sh
+++ 2048.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 # 2048 in POSIX Shell
-#
-# TODO:
-# high score
-# display score increase
 
 # relevant throughout
 # shellcheck disable=SC2046
@@ -14,28 +10,9 @@
 	# 'eval "$@"' is used for removing leading spaces
 
 # ensure a safe exit
-trap 'exit_program' INT QUIT TERM EXIT
-
-help() {
-	printf 'Usage: %s %s\n' "$0" '[options...]
- -h, --help       Print this
- -e, --exponent   Use exponents instead of powers
- -n, --no-color   No color mode
-
-Movement: wasd, hjkl, arrow keys, 8462 & ENTER
-Reset: r & ENTER
-Quit: q & ENTER
-Press a key for a direction and ENTER
-You can press multiple keys,
-but only the last pressed key is used'
-}
-
-reset_terminal() {
-	printf '\033c'
-}
+trap 'exit_program' INT QUIT TERM
 
 setup_terminal() {
-	reset_terminal
 	# hide cursor
 	printf '\033[?25l'
 	# clear screen
@@ -45,7 +22,15 @@ setup_terminal() {
 exit_program() {
 	# show cursor
 	printf '\033[?25h'
+
 	exit
+}
+
+clean_term() {
+	# go to line under the tiles
+	printf '\033[6;1H'
+	# erase display from cursor
+	printf '\033[J'
 }
 
 rand() {
@@ -53,6 +38,9 @@ rand() {
 	# "$1" must be (2^d)-1, so all digits are 1 in binary
 	# give random number from 0 to "$1"
 	# this is actually quite good & fast. didn't think it could be elegant
+	# I think the distribution is skewed towards smaller numbers,
+	# because the number of bytes read is by definition on avarage 255
+	# as the char 'newline' is 1 in 2^8
 
 	# /dev/random is not posix
 	read -r x < /dev/random
@@ -72,11 +60,11 @@ rewrite_pos_para() {
 
 	# select element of array and change it
 	n=1
-	for i; do
-		if [ "${n}" -eq "$pos" ]; then
+	while [ "$n" -le "$#" ]; do
+		if [ "$n" -eq "$pos" ]; then
 			array="$array $value"
 		else
-			array="$array $i"
+			array="$array \${$n}"
 		fi
 		n="$((n + 1))"
 	done
@@ -109,11 +97,11 @@ populate_tile() {
 		return
 	fi
 
-	# calculate which array element to populate
+	# which array element to populate
 	pos="$(($(rand 15) + 1))"
 
 	# if we selected an non-empty tile, do it again until we select an empty one
-	while eval [ \${$pos} -ne 0 ]; do
+	while eval [ "\${$pos}" -ne 0 ]; do
 		pos="$(($(rand 15) + 1))"
 	done
 
@@ -129,7 +117,11 @@ print() {
 	# print "$@"
 	# it prints the score and all tiles
 
-	echo "score: $score"
+	# go to top of terminal
+	printf '\033[1;1H'
+
+	printf 'score: %d %+6d\n' "$score" "$((score - score_prev))"
+
 	line=''
 
 	n=1
@@ -137,9 +129,9 @@ print() {
 		if [ "$power" -eq 1 ]; then
 			if [ "$color" -eq 1 ]; then
 				if [ "$i" -eq 0 ]; then
-					line="$line"$(printf '\033[7m\033[38;5;%dm    ' "$((i+5))")
+					line="$line"$(printf '\033[7m\033[38;5;%dm    ' "$((i + 5))")
 				else
-					line="$line"$(printf '\033[7m\033[38;5;%dm%4d' "$((i+5))" "$((1 << i))")
+					line="$line"$(printf '\033[7m\033[38;5;%dm%4d' "$((i + 5))" "$((1 << i))")
 				fi
 			else
 				if [ "$i" -ne 0 ]; then
@@ -150,9 +142,9 @@ print() {
 		else
 			if [ "$color" -eq 1 ]; then
 				if [ "$i" -eq 0 ]; then
-					line="$line"$(printf '\033[7m\033[38;5;%dm  ' "$((i+5))")
+					line="$line"$(printf '\033[7m\033[38;5;%dm  ' "$((i + 5))")
 				else
-					line="$line"$(printf '\033[7m\033[38;5;%dm%2d' "$((i+5))" "$i")
+					line="$line"$(printf '\033[7m\033[38;5;%dm%2d' "$((i + 5))" "$i")
 				fi
 			else
 				line="$line"$(printf '%-3d' "$i")
@@ -169,16 +161,8 @@ print() {
 		fi
 	done
 
+	# reset attributes
 	printf '\033[0m'
-}
-
-clean_term() {
-	# go to line under the tiles
-	printf '\033[6;1H'
-	# erase display from cursor
-	printf '\033[J'
-	# go to top of terminal
-	printf '\033[1;1H'
 }
 
 check_game_over() {
@@ -186,13 +170,13 @@ check_game_over() {
 	# check if in stalemate
 	# echo 1 is game over
 
-	# if all tiles are populated, the game is not over
+	# if not all tiles are populated, the game is not over
 	if [ $(check_populated "$@") -eq 0 ]; then
 		echo 0
 		return
 	fi
 
-	# check if no tile is mergable
+	# check if no tile is mergeable
 	# column
 	c=0
 	while [ "$c" -le 8 ]; do
@@ -201,13 +185,13 @@ check_game_over() {
 		while [ "$r" -le 3 ]; do
 
 			# check tile to right
-			if eval [ \${$((c + r))} -eq \${$((c + r + 1))} ]; then
+			if eval [ "\${$((c + r))}" -eq "\${$((c + r + 1))}" ]; then
 				echo 0
 				return
 			fi
 
 			# check tile below
-			if eval [ \${$((c + r))} -eq \${$((c + r + 4))} ]; then
+			if eval [ "\${$((c + r))}" -eq "\${$((c + r + 4))}" ]; then
 				echo 0
 				return
 			fi
@@ -231,7 +215,7 @@ check_game_state() {
 		# check for 2048
 		# 1 << 11 == 2048 == 2^11
 		if [ "$i" -eq 11 ]; then
-			echo 'You won!'
+			echo 'You won'
 			return
 		fi
 	done
@@ -249,17 +233,17 @@ move_up() {
 	# iterate each tiles
 	while [ "$n" -le 12 ]; do
 		# tile is 0
-		if eval [ \${$n} -eq 0 ]; then
+		if eval [ "\${$n}" -eq 0 ]; then
 			i=4
 			# check all tiles below
 			while [ "$((n + i))" -le 16 ]; do
 				# continue when tile is 0
-				if eval [ \${$((n + i))} -eq 0 ]; then
+				if eval [ "\${$((n + i))}" -eq 0 ]; then
 					i="$((i+4))"
 				# move tile if it is not 0
 				else
 					# copy value to new tile
-					set $(eval rewrite_pos_para "$n" \${$((n + i))} "$@")
+					set $(eval rewrite_pos_para "$n" "\${$((n + i))}" "$@")
 					# set old tile to 0
 					set $(rewrite_pos_para "$((n + i))" 0 "$@")
 					break
@@ -277,11 +261,11 @@ move_up() {
 	# check every mergeable tile
 	while [ "$n" -le 12 ]; do
 		# this condition must be seperate, it's dumb
-		if eval [ \${$n} -ne 0 ]; then
+		if eval [ "\${$n}" -ne 0 ]; then
 			# tile below has the same value
-			if eval [ \${$((n + 4))} -eq \${$n} ]; then
+			if eval [ "\${$((n + 4))}" -eq "\${$n}" ]; then
 				# val is used because \${$n}" can't be in arithmic expansion '$(())'
-				eval val=\${$n}
+				eval val="\${$n}"
 				# increase score
 				score="$((score + (1 << (val + 1))))"
 				# increase value of tile
@@ -292,17 +276,17 @@ move_up() {
 				# move all non-empty tiles below the merged merged one up
 				if [ "$n" -le 8 ]; then
 					# tile below the tile that got set to 0 is 0
-					if eval [ \${$((n + 8))} -ne 0 ]; then
+					if eval [ "\${$((n + 8))}" -ne 0 ]; then
 						i=8
 						# check all tiles below
 						while [ "$((n+i))" -le 16 ]; do
 							# continue when tile is 0
-							if eval [ \${$((n + i))} -eq 0 ]; then
+							if eval [ "\${$((n + i))}" -eq 0 ]; then
 								i="$((i + 4))"
 							# move tile if it is not 0
 							else
 								# copy value to new tile
-								set $(eval rewrite_pos_para "$((n + i - 4))" \${$((n + i))} "$@")
+								set $(eval rewrite_pos_para "$((n + i - 4))" "\${$((n + i))}" "$@")
 								# set old tile to 0
 								set $(rewrite_pos_para "$((n + i))" 0 "$@")
 							fi
@@ -324,13 +308,13 @@ move_down() {
 	# move
 	n=16
 	while [ "$n" -ge 4 ]; do
-		if eval [ \${$n} -eq 0 ]; then
+		if eval [ "\${$n}" -eq 0 ]; then
 			i=4
 			while [ "$((n - i))" -ge 1 ]; do
-				if eval [ \${$((n - i))} -eq 0 ]; then
+				if eval [ "\${$((n - i))}" -eq 0 ]; then
 					i="$((i + 4))"
 				else
-					set $(eval rewrite_pos_para "$n" \${$((n - i))} "$@")
+					set $(eval rewrite_pos_para "$n" "\${$((n - i))}" "$@")
 					set $(rewrite_pos_para "$((n - i))" 0 "$@")
 					break
 				fi
@@ -345,22 +329,22 @@ move_down() {
 
 	n=16
 	while [ "$n" -gt 4 ]; do
-		if eval [ \${$n} -ne 0 ]; then
-			if eval [ \${$((n - 4))} -eq \${$n} ]; then
-				eval val=\${$n}
+		if eval [ "\${$n}" -ne 0 ]; then
+			if eval [ "\${$((n - 4))}" -eq "\${$n}" ]; then
+				eval val="\${$n}"
 				score="$((score + (1 << (val + 1))))"
 				set $(rewrite_pos_para "$n" $((val + 1))  "$@")
 				set $(rewrite_pos_para "$((n - 4))" 0 "$@")
 
 				# move all non-empty tiles above the merged merged one down
 				if [ "$n" -gt 8 ]; then
-					if eval [ \${$((n - 8))} -ne 0 ]; then
+					if eval [ "\${$((n - 8))}" -ne 0 ]; then
 						i=8
 						while [ "$((n-i))" -ge 1 ]; do
-							if eval [ \${$((n - i))} -eq 0 ]; then
+							if eval [ "\${$((n - i))}" -eq 0 ]; then
 								i="$((i + 4))"
 							else
-								set $(eval rewrite_pos_para "$((n - i + 4))" \${$((n - i))} "$@")
+								set $(eval rewrite_pos_para "$((n - i + 4))" "\${$((n - i))}" "$@")
 								set $(rewrite_pos_para "$((n - i))" 0 "$@")
 							fi
 						done
@@ -388,17 +372,17 @@ move_left() {
 		# iterate each tile in col
 		while [ "$r" -le 3 ]; do
 			# tile is 0
-			if eval [ \${$((c + r))} -eq 0 ]; then
+			if eval [ "\${$((c + r))}" -eq 0 ]; then
 				i=1
 				# check all tiles to the right
 				while [ "$((r+i))" -le 4 ]; do
 					# continue when tile is 0
-					if eval [ \${$((c + r + i))} -eq 0 ]; then
+					if eval [ "\${$((c + r + i))}" -eq 0 ]; then
 						i="$((i + 1))"
 					# move tile if it is not 0
 					else
 						# copy value to new tile
-						set $(eval rewrite_pos_para "$((c + r))" \${$((c + r + i))} "$@")
+						set $(eval rewrite_pos_para "$((c + r))" "\${$((c + r + i))}" "$@")
 						# set old tile to 0
 						set $(rewrite_pos_para "$((c + r + i))" 0 "$@")
 						break
@@ -424,10 +408,10 @@ move_left() {
 		# iterate each tile in col
 		while [ "$r" -le 3 ]; do
 			# tile is not 0
-			if eval [ \${$((c + r))} -ne 0 ]; then
+			if eval [ "\${$((c + r))}" -ne 0 ]; then
 				# tile to the right has the same value
-				if eval [ \${$((c + r + 1))} -eq \${$((c + r))} ]; then
-					eval val=\${$((c + r))}
+				if eval [ "\${$((c + r + 1))}" -eq "\${$((c + r))}" ]; then
+					eval val="\${$((c + r))}"
 					# increase score
 					score="$((score + (1 << (val + 1))))"
 					# increase value of tile
@@ -437,13 +421,13 @@ move_left() {
 
 					# move all non-empty tiles right of the merged merged one left
 					if [ "$r" -le 2 ]; then
-						if eval [ \${$((c + r + 2))} -ne 0 ]; then
+						if eval [ "\${$((c + r + 2))}" -ne 0 ]; then
 							i=2
 							while [ "$((r + i))" -le 4 ]; do 
-								if eval [ \${$((c + r + i))} -eq 0 ]; then
+								if eval [ "\${$((c + r + i))}" -eq 0 ]; then
 									i="$((i + 1))"
 								else #move if tile is not 0
-									set $(eval rewrite_pos_para "$((c + r + i - 1))" \${$((c + r + i))} "$@")
+									set $(eval rewrite_pos_para "$((c + r + i - 1))" "\${$((c + r + i))}" "$@")
 									set $(rewrite_pos_para "$((c + r + i))" 0 "$@")
 								fi
 							done
@@ -469,13 +453,13 @@ move_right() {
 	while [ "$c" -le 12 ]; do
 		r=4
 		while [ "$r" -ge 1 ]; do
-			if eval [ \${$((c + r))} -eq 0 ]; then
+			if eval [ "\${$((c + r))}" -eq 0 ]; then
 				i=1
 				while [ "$((r - i))" -ge 1 ]; do
-					if eval [ \${$((c + r - i))} -eq 0 ]; then
+					if eval [ "\${$((c + r - i))}" -eq 0 ]; then
 						i="$((i + 1))"
 					else
-						set $(eval rewrite_pos_para "$((c + r))" \${$((c + r - i))} "$@")
+						set $(eval rewrite_pos_para "$((c + r))" "\${$((c + r - i))}" "$@")
 						set $(rewrite_pos_para "$((c + r - i))" 0 "$@")
 						break
 					fi
@@ -495,22 +479,22 @@ move_right() {
 	while [ "$c" -le 12 ]; do
 		r=4
 		while [ "$r" -gt 1 ]; do
-			if eval [ \${$((c + r))} -ne 0 ]; then
-				if eval [ \${$((c + r - 1))} -eq \${$((c + r))} ]; then
-					eval val=\${$((c + r))}
+			if eval [ "\${$((c + r))}" -ne 0 ]; then
+				if eval [ "\${$((c + r - 1))}" -eq "\${$((c + r))}" ]; then
+					eval val="\${$((c + r))}"
 					score="$((score + (1 << (val + 1))))"
 					set $(rewrite_pos_para "$((c + r))" $((val + 1))  "$@")
 					set $(rewrite_pos_para "$((c + r - 1))" 0 "$@")
 
 					# move all non-empty tiles left of the merged merged one right
 					if [ "$((c + r - 2))" -ge 1 ]; then
-						if eval [ \${$((c + r - 2))} -ne 0 ]; then
+						if eval [ "\${$((c + r - 2))}" -ne 0 ]; then
 							i=2
 							while [ "$((r - i))" -ge 1 ]; do
-								if eval [ \${$((c + r - i))} -eq 0 ]; then
+								if eval [ "\${$((c + r - i))}" -eq 0 ]; then
 									i="$((i + 1))"
 								else
-									set $(eval rewrite_pos_para "$((c + r - i + 1))" \${$((c + r - i))} "$@")
+									set $(eval rewrite_pos_para "$((c + r - i + 1))" "\${$((c + r - i))}" "$@")
 									set $(rewrite_pos_para "$((c + r - i))" 0 "$@")
 								fi
 							done
@@ -529,7 +513,83 @@ move_right() {
 	echo "$score" "$@"
 }
 
-init() {
+load_highscore() {
+	if [ ! -r "$highscore_file" ]; then
+		return
+	fi
+	read -r s < "$highscore_file"
+
+	# select everything after the last ': '
+	highscore="${s##*: }"
+	# select everything before the last ': '
+	highscore_name="${s%%: *}"
+}
+
+save_highscore() {
+	if [ "$score" -ge "$highscore" ]; then
+		printf 'Old highscore: %d by %s\nNew highscore: %d\n' "$highscore" "$highscore_name" "$score"
+		printf 'Enter name to override, or [n/no/] to discard\n'
+
+		read -r s
+
+		case "$s" in
+			n | no | '') ;; # discard
+			*) echo "$s: $score" > "$highscore_file" ;; # save
+		esac
+	fi
+}
+
+help() {
+	printf 'Usage: %s %s\n' "$0" '[options...]
+ -h, --help                Print this
+ -e, --exponent            Use exponents instead of powers
+ -n, --no-color            No color mode
+ -f, highscore-file=<file> Highscore-file. Default is highscore.txt
+
+Movement: wasd, hjkl, arrow keys, 8462 & ENTER
+Reset: r & ENTER
+Quit: q & ENTER, Ctrl-C to exit without saving highscore
+Press a key for a direction and ENTER
+You can press multiple keys,
+but only the last pressed key is used'
+}
+
+handle_options() {
+	while [ "$#" -gt 0 ]; do
+		case "$1" in
+			-h | --help)
+				help
+				exit
+				;;
+			-n | --no-color | --no-colour)
+				color=0
+				;;
+			-e | --exponent)
+				power=0
+				;;
+			-f)
+				shift
+				highscore_file="$1"
+				;;
+			'--highscore-file='*)
+				highscore_file="${1#*=}"
+				;;
+			*)
+				;;
+		esac
+		shift
+	done
+}
+
+check_dev_random() {
+	# if /dev/random not readable
+	if [ ! -r '/dev/random' ]; then
+		printf '%s\n' '/dev/random is not readable. It can be replaced with another rng file'
+		exit 1
+	fi
+}
+
+init_tiles() {
 	# set init
 
 	set 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
@@ -540,41 +600,29 @@ init() {
 	echo "$@"
 }
 
+init() {
+	score=0
+	score_prev=0
+	highscore=0
+	highscore_name=''
+	moves=0
 
-# handle options
-color=1
-power=1
-for i; do
-	case "$i" in
-		-h | --help)
-			help
-			exit
-			;;
-		-n | --no-color | --no-colour)
-			color=0
-			;;
-		-e | --exponent)
-			power=0
-			;;
-	esac
-done
-
-# if /dev/random not readable
-if [ ! -r '/dev/random' ]; then
-	printf '%s\n' '/dev/random is not readable
-it can be replaced with another rng file'
-	exit 1
-fi
-
+	load_highscore
+	setup_terminal
+	print "$@"
+}
 
 main() {
-	setup_terminal
+	color=1
+	power=1
+	highscore_file=highscore.txt
 
-	score=0
-	set $(init)
-	print "$@"
+	handle_options "$@"
+	check_dev_random
 
-	moves=0
+	set $(init_tiles)
+	init "$@"
+
 	while read -r input; do
 
 		# "${input%?}" removes last char
@@ -601,16 +649,12 @@ main() {
 				shift
 				;;
 			r)
-				setup_terminal
-
-				score=0
-				set $(init)
-				print "$@"
-
-				moves=0
+				set $(init_tiles)
+				init "$@"
 				continue
 				;;
 			q)
+				save_highscore
 				exit_program
 				;;
 			*)
@@ -619,19 +663,20 @@ main() {
 				;;
 		esac
 
-		clean_term
-
 		set $(populate_tile "$@")
 		print "$@"
+		clean_term
+		score_prev="$score"
 		moves="$((moves + 1))"
 
 		# end game
 		return_vars=$(check_game_state "$@")
-		if [ "${#return_vars}" -ne 0 ]; then
-			echo "$return_vars"
+		if [ "$return_vars" ]; then
+			echo "$return_vars in $moves moves"
+			save_highscore
 			exit_program
 		fi
 	done
 }
 
-main
+main "$@"
